2676eecce9135104bdf30c112bf6ad522e9f06d3
SomeoneScripts <lucasviniciussoaresdasilva50@gmail.com>
2025-10-14 19:17:08 -0300

    Add files via upload

---
+++ Translation System/Languages/Portuguese.lua
@@ -0,0 +1,39 @@
+return {
+	-- Others --
+	["Someone Hub | Blox Fruits"] = "Someone Hub | Blox Fruits",
+	["By Someone"] = "Por Someone",
+	["Information"] = "Informações",
+	["Farm"] = "Farmar",
+	["Bosses"] = "Chefes",
+	["Items"] = "Itens",
+	["Quests"] = "Missões",
+	["Fishing"] = "Pescar",
+	["Sea Events"] = "Eventos Marítimos",
+	["Islands"] = "Ilhas",
+	["Fruits"] = "Frutas",
+	["Raid/Upgrade"] = "Raid/Melhorias",
+	["Teleport"] = "Teletransporte",
+	["Player"] = "Jogador",
+	["Race"] = "Raça",
+	["Shop"] = "Comprar",
+	["Visual"] = "Visual",
+	["Settings"] = "Configurações",
+	-- Information --
+	["Social Media"] = "Redes Sociais",
+	["Script Community; There You Can Find People And More Information About The Script"] = "Comunidade Do Script; Lá Você Pode Encontrar Pessoas E Mais Informações Sobre O Script",
+	-- Settings --
+	-- Farm --
+	-- Bosses --
+	-- Items --
+	-- Quests --
+	-- Fishing --
+	-- SeaEvents --
+	-- Islands --
+	-- Fruits --
+	-- Raid --
+	-- Teleport --
+	-- Player --
+	-- Race --
+	-- Shop --
+	-- Visual --
+}
