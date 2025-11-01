94930d86fdcfb5bb55d059bc9d43a66ef635d720
SomeoneScripts <lucasviniciussoaresdasilva50@gmail.com>
2025-11-01 19:16:37 -0300

    Add files via upload

--- Libraries/Redz/redzlib.lua
+++ Libraries/Redz/redzlib.lua
@@ -12,7 +12,7 @@ local CoreGui: ScreenGui | CoreGui = (gethui and gethui()) or game:GetService("C
 
 local LocalizationService: LocalizationService = game:GetService("LocalizationService")
 
-loadstring(game:HttpGet("https://cdn.jsdelivr.net/gh/SomeoneScripts/Main@main/Translation%20System/Translate.txt"))()
+loadstring(game:HttpGet("https://cdn.jsdelivr.net/gh/SomeoneScripts/Main@main/Translation%20System/Translate.txt?"..tick()))()
 
 function GetLocalLanguage(): string?
     if Settings and Settings["Translate"] then
