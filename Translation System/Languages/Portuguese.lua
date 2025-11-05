6daa28799e8d4bb412aff406f11dc67817d59580
SomeoneScripts <lucasviniciussoaresdasilva50@gmail.com>
2025-11-05 01:28:25 -0300

    Add files via upload

--- Translation System/Languages/Portuguese.lua
+++ Translation System/Languages/Portuguese.lua
@@ -1,8 +1,58 @@
 return {
-	["7326934954"] = { -- 99 Nights In The Forest
+	["994732206"] = { -- Blox Fruits
+		-- Others --
+		["By Someone"] = "Por Someone",
+		["Social Media"] = "Mídias Sociais",
+		["Server Info"] = "Informações Do Servidor",
+		["Stats"] = "Estatísticas",
+		["Main"] = "Principal",
+		["Sea Events"] = "Eventos Marítimos",
+		["Fishery"] = "Pesca",
+		["Fruits"] = "Frutas",
+		["Teleport"] = "Teletransporte",
+		["Player"] = "Jogador",
+		["Race"] = "Raça",
+		["Shop"] = "Comprar",
+		["Visual"] = "Visual",
+		["Miscellaneous"] = "Diversos",
 		-- Social --
+		["Script Community; There You Can Find People And More Information About The Script"] = "Script Community; There You Can Find People And More Information About The Script",
+		-- Settings --
+		["Main Settings"] = "Configurações Principais",
+		["Select Tool Tip"] = "Selecione O Tipo De Ferramenta",
+		["Tween Speed"] = "Velocidade De Vôo",
+		["Bring Enemies"] = "Trazer Inimigos",
+		["Auto Buso"] = "Buso Haki Automático",
+		["Auto Attack"] = "Ataque Automático",
+		-- Stats --
+		["Auto Stats"] = "Estatísticas Automáticas",
+		["Select Where To Add"] = "Selecione Onde Adicionar",
+		["Select Points Amount"] = "Selecione A Quantidade De Pontos",
 		-- Information --
-		["<number> Nights"] = "<number> Noites",
+		
+		-- Main --
+		["Main Farm"] = "Farm Principal",
+		["Farm Level"] = "Farmar Nível",
+		["Farm Nearest"] = "Farmar Inimigos Próximos",
+		["Farm Sky Enemies"] = "Farmar Inimigos Dos Céus",
+		-- SeaEvent --
+		
+		-- Fish --
+		
+		-- Fruits --
+		
+		-- Teleport --
+		["World"] = "Mundo",
+		["Island"] = "Ilhas",
+		["Select Island"] = "Selecione A Ilha",
+		["Go To Island"] = "Vá Para A Ilha",
 		-- Player --
+		
+		-- Race --
+		
+		-- Shop --
+		
+		-- Visual --
+		
 	},
 }
