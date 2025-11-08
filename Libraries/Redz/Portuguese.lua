86b6b9eedbd378c5a3b7ff8ef8e14733be0d5402
SomeoneScripts <lucasviniciussoaresdasilva50@gmail.com>
2025-11-08 16:46:30 -0300

    Add files via upload

---
+++ Libraries/Redz/Portuguese.lua
@@ -0,0 +1,92 @@
+return {
+	["994732206"] = { -- Blox Fruits
+		-- Others --
+		["By Someone"] = "Por Someone",
+		["Social Media"] = "Mídias Sociais",
+		["Server Info"] = "Informações Do Servidor",
+		["Stats"] = "Estatísticas",
+		["Main"] = "Principal",
+		["Sea Events"] = "Eventos Marítimos",
+		["Fishery"] = "Pesca",
+		["Fruits"] = "Frutas",
+		["Teleport"] = "Teletransporte",
+		["Player"] = "Jogador",
+		["Race"] = "Raça",
+		["Shop"] = "Comprar",
+		["Visual"] = "Visual",
+		["Miscellaneous"] = "Diversos",
+		["Angel Wings"] = "Asas De Anjo",
+		["Fish Tail"] = "Cauda De Peixe",
+		["Leather & Scrap Metal"] = "Couro E Sucata",
+		["Magma Ore"] = "Minério De Magma",
+		["Mystic Droplet"] = "Gota Mística",
+		["Radioactive Material"] = "Material Radioativo",
+		["Vampire Fang"] = "Presa De Vampiro",
+		["Conjured Cocoa"] = "Cacau Conjurado",
+		["Demonic Wisp"] = "Essência Demoníaca",
+		["Dragon Scale"] = "Escama De Dragão",
+		["Gunpowder"] = "Pólvora",
+		["Mini Tusk"] = "Presinha",
+		["Nightmare Catcher"] = "Apanhador De Pesadelos",
+		["The Gorilla King"] = "O Rei Gorila",
+		["Chef"] = "Chefe",
+		["The Saw"] = "A Serra",
+		["Mob Leader"] = "Lider Da Máfia",
+		["Vice Admiral"] = "Vice-almirante",
+		["Warden"] = "Guarda",
+		-- Social --
+		["Script Community; There You Can Find People And More Information About The Script"] = "Comunidade Do Script; Lá Você Pode Encontrar Pessoas E Mais Informações Sobre O Roteiro.",
+		-- Settings --
+		["Main Settings"] = "Configurações Principais",
+			["Select Tool Tip"] = "Selecione O Tipo De Ferramenta",
+			["Tween Speed"] = "Velocidade De Vôo",
+			["Bring Enemies"] = "Trazer Inimigos",
+			["Auto Buso"] = "Buso Haki Automático",
+			["Auto Attack"] = "Ataque Automático",
+		-- Stats --
+		["Auto Stats"] = "Estatísticas Automáticas",
+			["Select Where To Add"] = "Selecione Onde Adicionar",
+			["Select Points Amount"] = "Selecione A Quantidade De Pontos",
+		-- Information --
+		
+		-- Main --
+		["Main Farm"] = "Farm Principal",
+			["Farm Level"] = "Farmar Nível",
+			["Farm Nearest"] = "Farmar Inimigos Próximos",
+			["Farm Sky Enemies"] = "Farmar Inimigos Dos Céus",
+			["Factory Raid"] = "Ataque A Fábrica",
+			["Pirate Raid"] = "Ataque Os Piratas",
+		["Bosses"] = "Chefes",
+			["Update Bosses List"] = "Atualizar Lista De Chefes",
+			["Select Boss"] = "Selecione Um Chefe",
+			["Defeat Selected Boss"] = "Derrotar O Chefe Selecionado",
+			["Defeat All Bosses"] = "Derrotar Todos Os Chefes",
+			["Change Server"] = "Trocar De Servidor",
+		["Chests"] = "Baús",
+			["Collect Chests"] = "Coletar Baús",
+		["Ectoplasms"] = "Ectoplasmas",
+			["Farm Ectoplasms"] = "Farmar Ectoplasmas",
+		["Material"] = "Material",
+			["Select Material"] = "Selecione O Material",
+			["Farm Material"] = "Farmar Material",
+		-- SeaEvent --
+		
+		-- Fish --
+		
+		-- Fruits --
+		
+		-- Teleport --
+		["World"] = "Mundo",
+		["Island"] = "Ilhas",
+			["Select Island"] = "Selecione A Ilha",
+			["Go To Island"] = "Vá Para A Ilha",
+		-- Player --
+		
+		-- Race --
+		
+		-- Shop --
+		
+		-- Visual --
+		
+	},
+}
