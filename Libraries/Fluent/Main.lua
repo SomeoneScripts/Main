61b8cd7e2b8f8c8c77b68ebff9cf527a06cbf383
SomeoneScripts <lucasviniciussoaresdasilva50@gmail.com>
2025-11-11 18:23:39 -0300

    Delete Libraries/Fluent/Main.lua

--- Libraries/Fluent/Main.lua
+++
@@ -1,7 +0,0 @@
-Ids = {
-	["7326934954"] = "https://raw.githubusercontent.com/SomeoneScripts/Main/refs/heads/main/Games/99%20Nights%20In%20The%20Forest/99NightsInTheForest.lua",
-	["994732206"] = "https://raw.githubusercontent.com/SomeoneScripts/Main/refs/heads/main/Games/Blox%20Fruits/BloxFruitsDefault.lua",
-}
-if Ids[tostring(game.GameId)] then
-loadstring(game:HttpGet(Ids[tostring(game.GameId)]))()
-end
