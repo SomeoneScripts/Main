de38f78c1cf6de67c3f90165d2a2905efeb044bd
SomeoneScripts <lucasviniciussoaresdasilva50@gmail.com>
2025-11-25 21:18:14 -0300

    Add files via upload

---
+++ TranslationSystem/Languages/Portuguese/994732206.lua
@@ -0,0 +1,15 @@
+	return {-- Blox Fruits
+	    	-- Info --
+		    -- Main --
+			-- Quests --
+			-- Fish --
+			-- Sea Events --
+			-- Islands --
+			-- Fruits --
+			-- Teleport --
+			-- Player --
+			-- Race --
+			-- Shop --
+			-- Visual --
+			-- Settings --
+    }
