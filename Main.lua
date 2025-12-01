ebf617ba0ad34d51ce15ced5ec4b560f08492d6a
SomeoneScripts <lucasviniciussoaresdasilva50@gmail.com>
2025-12-01 18:07:06 -0300

    Add files via upload

--- Main.lua
+++ Main.lua
@@ -1,7 +1,8 @@
-Ids = {
-	["7326934954"] = "https://raw.githubusercontent.com/SomeoneScripts/Main/refs/heads/main/Games/99%20Nights%20In%20The%20Forest/99NightsInTheForest.lua",
-	["994732206"] = "https://raw.githubusercontent.com/SomeoneScripts/Main/refs/heads/main/Games/Blox%20Fruits/BloxFruitsDefault.lua",
+local URL = "https://raw.githubusercontent.com/SomeoneScripts/Main/refs/heads/main/Games/"
+local Ids = {
+	["994732206"] = URL.."BloxFruits/BloxFruits.lua",
 }
+
 if Ids[tostring(game.GameId)] then
-loadstring(game:HttpGet(Ids[tostring(game.GameId)]))()
+	loadstring(game:HttpGet(Ids[tostring(game.GameId)]))()
 end
