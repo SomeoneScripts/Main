5c784fe4d897b56a5fda79545c5f8fb4cb4c38da
SomeoneScripts <lucasviniciussoaresdasilva50@gmail.com>
2025-12-10 18:57:45 -0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,7 @@
 local URL = "https://raw.githubusercontent.com/SomeoneScripts/Main/refs/heads/main/Games/"
 local Ids = {
-	["994732206"] = URL.."BloxFruits/BloxFruits.lua",
+	["994732206"] = URL.."BloxFruits.lua",
+	["7120911607"] = URL.."After3Am.lua",
 }
 
 if Ids[tostring(game.GameId)] then
