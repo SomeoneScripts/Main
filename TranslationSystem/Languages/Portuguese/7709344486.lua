5402f013f1811c3aa488d651d267f1fca3665a8a
SomeoneScripts <lucasviniciussoaresdasilva50@gmail.com>
2025-12-10 18:49:28 -0300

    Delete TranslationSystem/Languages/Portuguese/7709344486.lua

--- TranslationSystem/Languages/Portuguese/7709344486.lua
+++
@@ -1,8 +0,0 @@
-return { -- Steal A Brainrot 
-	["Vortex Hub - Steal A Brainrot"] = "Vortex Hub - Roube Um Brainrot",
-	-- Player --
-	["Infinite Jump"] = " Pulo Infinito",
-	-- Visual --
-	["Extra Sensory Perception"] = "Percepção Extra Sensorial",
-	["ESP Players"] = " ESP Jogadores",
-}
