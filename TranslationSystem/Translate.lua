e31305f1e9e03aefee318697c23cf9a551a8dc58
SomeoneScripts <lucasviniciussoaresdasilva50@gmail.com>
2025-12-14 03:38:23 -0300

    Update Translate.lua

--- TranslationSystem/Translate.lua
+++ TranslationSystem/Translate.lua
@@ -8,11 +8,12 @@
 	
 	for _, Lang in ipairs(Translations.Languages) do
 	    local ok, data = pcall(function()
-	        local url = "https://raw.githubusercontent.com/SomeoneScripts/Main/refs/heads/main/TranslationSystem/Languages/"..Lang.."/"..Lang..".lua"
+	        local url = "https://raw.githubusercontent.com/SomeoneScripts/Main/refs/heads/main/TranslationSystem/Languages/"..Lang..".lua"
 	        return loadstring(game:HttpGet(url))()
 	    end)
 	
 	    if ok and data then
 	        Translations[Lang] = data
 	    end
+
 	end
