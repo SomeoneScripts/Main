9a2c6e89b5f635172939d34e79890f9e9dbe7832
SomeoneScripts <lucasviniciussoaresdasilva50@gmail.com>
2025-12-24 07:36:58 -0300

    Update Translate.lua

--- TranslationSystem/Translate.lua
+++ TranslationSystem/Translate.lua
@@ -3,14 +3,8 @@
 	        "Portuguese",
 	        "Thai",
 	        "Vietnamese"
-	    },
-		Flags = {
-			["Portuguese"] = "5826570404",
-	        ["Thai"] = "105309709443809",
-	        ["Vietnamese"] = "9041883110",
-		},
+	},
 	}
-	
 	for _, Lang in ipairs(Translations.Languages) do
 	    local ok, data = pcall(function()
 	        local url = "https://raw.githubusercontent.com/SomeoneScripts/Main/refs/heads/main/TranslationSystem/Languages/"..Lang..".lua"
@@ -22,4 +16,5 @@
 	    end
 
 	end
+
 
