b0a31a4ecd1f1203b7862d509d0840f672852d37
SomeoneScripts <lucasviniciussoaresdasilva50@gmail.com>
2026-01-02 21:06:55 -0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,10 +1,11 @@
 local Url = "https://raw.githubusercontent.com/SomeoneScripts/Main/refs/heads/main/Games/"
 local Ids = {
-	["6931042565"] = Url.."BloxFruits.lua",
+	["994732206"] = Url.."BloxFruits.lua",
 }
 
 if Ids[tostring(game.GameId)] then
 	loadstring(game:HttpGet(Ids[tostring(game.GameId)]))()
 end
+
 
 
